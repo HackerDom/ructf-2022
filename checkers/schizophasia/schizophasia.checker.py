2c95a70b5609fd7bbb4b865f2f0c0686f81efa2d
usernamedt <usernamedt@protonmail.ch>
2022-05-21 17:19:02 +0300

    update checker

--- checkers/schizophasia/schizophasia.checker.py
+++ checkers/schizophasia/schizophasia.checker.py
@@ -58,47 +58,73 @@ class PgErrorChecker:
         return True
 
 
+class PgConn:
+    def __init__(self, hostname):
+        self.hostname = hostname
+
+    def __enter__(self):
+        self.conn = pg8000.connect("svcuser", host=self.hostname, password="svcpass", database="postgres", port=PG_PORT)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if self.conn:
+            self.conn.close()
+
+
+def get_pg_conn(hostname):
+    return
+
+
 @checker.define_check
 async def check_service(request: CheckRequest) -> Verdict:
-    with ErrorChecker() as doctor_ec:
-        # check doctor is alive
-        url = f"http://{request.hostname}:{DOCTOR_PORT}/api/v1/jobs"
-        payload = {
-            'id': '4C3443283C07611254AECD1FDDFA99452AF996EC5819784B69C053B66F4F7F5EA22B7E27B33F7C93C492421D21DBD29FCB0819B4B1AFD9D2548CC831C47C57FCBBFF1CE70E3468E692B7355C02C85EB7812F93F7A6A0905C6730904E2CE27F34A8A018DA1025968453BC73047BC43EC90B7FFE323060CA3D5E1A28D9BB8AA337'}
-        files = []
-        headers = {}
-
-        resp = requests_with_retries().put(
-            url,
-            headers=headers,
-            data=payload
-        )
-        if resp is None:
-            return Verdict.CORRUPT("corrupt response from doctor")
-
-        resp_json = resp.json()
-        if "data" not in resp_json:
-            doctor_ec.verdict = Verdict.CORRUPT("corrupt response from doctor")
-        else:
-            doctor_ec.verdict = Verdict.OK()
-
     with PgErrorChecker() as registry_ec:
-        conn = pg8000.connect("svcuser", host=request.hostname, password="svcpass", database="postgres", port=PG_PORT)
-        cursor = conn.cursor()
-        cursor.execute("SELECT add_job('%s')" % str(uuid.uuid4()))
-        conn.commit()
-        conn.close()
+        with PgConn(request.hostname) as pgc:
+            cursor = pgc.conn.cursor()
+            cursor.execute("SELECT 1")
+            pgc.conn.commit()
 
-    if registry_ec.verdict._code > doctor_ec.verdict._code:
-        return registry_ec.verdict
-
-    return doctor_ec.verdict
+    return registry_ec.verdict
 
 
 @checker.define_vuln("flag_id is a metadata")
 class OracleChecker(VulnChecker):
     @staticmethod
     def put(request: PutRequest) -> Verdict:
+        # with ErrorChecker() as doctor_ec:
+        #     # check doctor is alive
+        #     url = f"http://{request.hostname}:{DOCTOR_PORT}/api/v1/jobs"
+        #     payload = {
+        #         'id': '4C3443283C07611254AECD1FDDFA99452AF996EC5819784B69C053B66F4F7F5EA22B7E27B33F7C93C492421D21DBD29FCB0819B4B1AFD9D2548CC831C47C57FCBBFF1CE70E3468E692B7355C02C85EB7812F93F7A6A0905C6730904E2CE27F34A8A018DA1025968453BC73047BC43EC90B7FFE323060CA3D5E1A28D9BB8AA337'}
+        #     files = []
+        #     headers = {}
+        #
+        #     resp = requests_with_retries().put(
+        #         url,
+        #         headers=headers,
+        #         data=payload
+        #     )
+        #     if resp is None:
+        #         return Verdict.CORRUPT("corrupt response from doctor")
+        #
+        #     resp_json = resp.json()
+        #     if "data" not in resp_json:
+        #         doctor_ec.verdict = Verdict.CORRUPT("corrupt response from doctor")
+        #     else:
+        #         doctor_ec.verdict = Verdict.OK()
+        #
+        # with PgErrorChecker() as registry_ec:
+        #     conn = pg8000.connect("svcuser", host=request.hostname, password="svcpass", database="postgres",
+        #                           port=PG_PORT)
+        #     cursor = conn.cursor()
+        #     cursor.execute("SELECT add_job('%s')" % str(uuid.uuid4()))
+        #     conn.commit()
+        #     conn.close()
+        #
+        # if registry_ec.verdict._code > doctor_ec.verdict._code:
+        #     return registry_ec.verdict
+        #
+        # return doctor_ec.verdict
+
         return Verdict.OK()
 
     @staticmethod
