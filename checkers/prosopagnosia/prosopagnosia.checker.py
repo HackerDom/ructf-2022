f89f7a8d191442fdaf6b228d68460e1ecf883c1a
rkhapov <rkhapov@yandex-team.ru>
2022-05-22 13:42:56 +0500

    prosopagnosia fix cheker

--- checkers/prosopagnosia/prosopagnosia.checker.py
+++ checkers/prosopagnosia/prosopagnosia.checker.py
@@ -84,9 +84,6 @@ class Base64Vuln(VulnChecker):
 
             with requests_with_retries().get(f'http://{req.hostname}:{PORT}/api/demo',
                                              headers={KEY_HEADER: key, NAME_HEADER: name}) as r:
-                if r.status_code == 404:
-                    return Verdict.CORRUPT('wrong flag')
-
                 if r.status_code != 200:
                     return Verdict.MUMBLE('wrong http code')
 
@@ -102,7 +99,7 @@ class Base64Vuln(VulnChecker):
 
             with requests_with_retries().get(f'http://{req.hostname}:{PORT}/{rom_path}') as r:
                 if r.status_code != 200:
-                    return Verdict.CORRUPT('wrong http code')
+                    return Verdict.MUMBLE('wrong http code')
 
                 if r.content != demo:
                     print(demo)
